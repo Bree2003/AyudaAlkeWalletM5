52eaf62e22b06ea91b6d871e79f5ca6ecff2e751
Bree2003 <breeale2003@gmail.com>
2024-05-09 16:49:34 -0400

    agregado archivo mysql para la bd

---
+++ script-mysql/Script-7.sql
@@ -0,0 +1,12 @@
+create database if not exists ejemploalke;
+
+use ejemploalke;
+
+create table if not exists usuarios(
+id int primary key auto_increment,
+nombre varchar(20) not null,
+apellido varchar(20) not null,
+correo varchar(25) not null,
+contrasena varchar(20) not null,
+saldo decimal(10,2) not null
+);
